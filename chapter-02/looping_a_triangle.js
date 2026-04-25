76a5d9a5cef91c32f73703485ac6111132ccd63a
GRRaul <rgr.negocio@gmail.com>
2026-04-25 15:29:08 +0000

    complete chapter 2 looping a triangle

---
+++ chapter-02/looping_a_triangle.js
@@ -0,0 +1,20 @@
+
+// Exercise 01: Looping a Triangle
+// This programme builds a triangle using the "#" character.
+// It starts with one "#", prints it to the console, and then adds another "#"
+// on each loop iteration.
+// The loop continues while the string length is less than or equal to 7.
+// Build and print a triangle by increasing the string length on each loop iteration.
+
+let text = '';
+
+while (text.length < 7 ) {
+    console.log(
+        text += '#'
+    )
+}
+
+
+   
+
+
